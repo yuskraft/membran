4eb2cc10c34781720fdc55981444a87812b93ef7
Nurlan Yusifli <nurlanyusifli10@gmail.com>
2026-03-02 17:38:26 +0400

    Initial tauri config

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import styles from './App.module.css';
+
+function App() {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>membran</h1>
+      <p className={styles.subtitle}>layer I</p>
+    </div>
+  );
+}
+
+export default App;
